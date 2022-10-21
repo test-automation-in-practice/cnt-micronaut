c2a3d75ea411604b5e5e30976ffbddc82967e67e
Silvio D'Alessandro <silvio.dalessandro@novatec-gmbh.de>
2022-10-21 09:57:29 +0200

    Add http-server module

---
+++ examples/http-server/src/test/kotlin/example/micronaut/HttpServerTest.kt
@@ -0,0 +1,20 @@
+package example.micronaut
+
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import jakarta.inject.Inject
+
+@MicronautTest
+class HttpServerTest {
+
+    @Inject
+    lateinit var application: EmbeddedApplication<*>
+
+    @Test
+    fun testItWorks() {
+        Assertions.assertTrue(application.isRunning)
+    }
+
+}
