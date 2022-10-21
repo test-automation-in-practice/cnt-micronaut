23baecb01b79703e274d4df237a3409c617353b4
Silvio D'Alessandro <silvio.dalessandro@novatec-gmbh.de>
2022-10-21 12:24:03 +0200

    Assertions done with RestAssured

--- examples/http-server/src/test/kotlin/example/micronaut/httpserver/port/BookResourceTest.kt
+++ examples/http-server/src/test/kotlin/example/micronaut/httpserver/port/BookResourceTest.kt
@@ -2,11 +2,10 @@ package example.micronaut.httpserver.port
 
 import example.micronaut.httpserver.Examples
 import example.micronaut.httpserver.books.core.BookCollection
-import io.kotest.assertions.assertSoftly
-import io.kotest.matchers.shouldBe
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import io.restassured.specification.RequestSpecification
 import jakarta.inject.Inject
+import org.hamcrest.CoreMatchers.equalTo
 import org.junit.jupiter.api.Test
 import java.util.*
 
@@ -19,18 +18,14 @@ internal class BookResourceTest {
     @Test
     fun `finding a Book by its ID returns 200 with found Book`(spec: RequestSpecification) {
         val bookRecord = bookCollection.add(Examples.book_cleanCode)
-        val bookRepresentation = spec.`when`().get("/books/${bookRecord.id}")
+        spec.get("/books/${bookRecord.id}")
             .then()
             .statusCode(200)
-            .extract().`as`(BookRepresentation::class.java)
-
-        assertSoftly(bookRepresentation) {
-            with(it) {
-                id shouldBe bookRecord.id
-                title shouldBe bookRecord.book.title
-                isbn shouldBe bookRecord.book.isbn
-            }
-        }
+            .body(
+                "id", equalTo(bookRecord.id.toString()),
+                "title", equalTo(bookRecord.book.title),
+                "isbn", equalTo(bookRecord.book.isbn)
+            )
     }
 
     @Test
