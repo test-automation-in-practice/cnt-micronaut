6e7ff13c16a1a4a4755fb4060227e45e4e9f6092
Jochen Just <jochen.just@novatec-gmbh.de>
2022-10-21 13:24:59 +0200

    Api provides a POST endpoint

--- examples/http-server/src/main/kotlin/example/micronaut/httpserver/api/BookResource.kt
+++ examples/http-server/src/main/kotlin/example/micronaut/httpserver/api/BookResource.kt
@@ -1,14 +1,20 @@
 package example.micronaut.httpserver.api
 
+import example.micronaut.httpserver.books.core.Book
 import example.micronaut.httpserver.books.core.BookCollection
+import example.micronaut.httpserver.books.core.BookRecord
 import io.micronaut.http.HttpResponse
+import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.PathVariable
+import io.micronaut.http.annotation.Post
+import io.micronaut.http.annotation.Status
 import java.util.*
+import javax.validation.Valid
 
 
-@Controller("/books")
+@Controller("/default-api/books")
 class BookResource(private val bookCollection: BookCollection) {
 
     @Get("/{bookId}")
@@ -16,9 +22,16 @@ class BookResource(private val bookCollection: BookCollection) {
         val bookRecord = bookCollection.get(UUID.fromString(bookId))
 
         return if (bookRecord != null) {
-            HttpResponse.ok(bookRecord.toRepresentation())
+            HttpResponse.ok(BookRepresentation(bookRecord))
         } else {
             HttpResponse.notFound<Void>()
         }
+    }
+
+    @Post
+    @Status(HttpStatus.CREATED)
+    fun post(@Valid createBookRequest: CreateBookRequest): HttpResponse<BookRepresentation> {
+        val bookRecord = bookCollection.add(Book(createBookRequest.isbn, createBookRequest.title))
+        return HttpResponse.created(BookRepresentation(bookRecord))
     }
 }
