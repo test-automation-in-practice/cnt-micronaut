207192a4b65e06f6497be47b6d1748d062a40527
Jochen Just <jochen.just@novatec-gmbh.de>
2023-09-07 11:00:53 +0200

    Migration to Micronaut 4 for project examples/caching

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,6 @@ rootProject.name="cnt-micronaut"
 
 include("examples:basics")
 include("examples:http-server")
-//include("examples:caching")
+include("examples:caching")
 //include("examples:graphql")
 include("examples:rabbitmq")
